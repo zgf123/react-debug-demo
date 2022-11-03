ea447ff3498b716a0aef3d98e299e554075ba2f4
zhangguangfei <zhangguangfei@bizseer.com>
2022-11-03 20:39:17 +0800

    debug env

--- config/env.js
+++ config/env.js
@@ -96,6 +96,10 @@ function getClientEnvironment(publicUrl) {
       env[key] = JSON.stringify(raw[key]);
       return env;
     }, {}),
+    // 加上
+  	__DEV__: true,
+    __EXPERIMENTAL__: true,
+    __PROFILE__: true
   };
 
   return { raw, stringified };
--- src/react/packages/shared/ReactSharedInternals.js
+++ src/react/packages/shared/ReactSharedInternals.js
@@ -6,10 +6,12 @@
  *
  * @flow
  */
+// 注释这段
+// import * as React from 'react';
 
-import * as React from 'react';
+// const ReactSharedInternals =
+//   React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
-const ReactSharedInternals =
-  React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-
+// 添加这段
+import ReactSharedInternals from '../react/src/ReactSharedInternals'
 export default ReactSharedInternals;
